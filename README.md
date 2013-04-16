3fb5ca91eaec7111d441f516abfe6c5871dd2d5b
Thomas VIAL <github@ifusio.com>
2013-04-16 16:22:35 +0200

    v0.1.2 => Removed isitdeployed.yml config file to make setup easier. Now 2 we have a 2 steps setup (against 4 before)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This plugin works as an interface between your [Capistrano](http://capistranorb.com/) deployments and [www.isitdeployed.com](http://www.isitdeployed.com) which gives you :
 * deployments counter for your projects
 * a deployment status page for you and your team which displays current deploy status (does not nead page reload for updates)
-* soon : deploys statistics
+* soon: deploys statistics
 
 And it's Free.
 
@@ -12,11 +12,16 @@ If not, you should! :-)
 
 Has been tested with Rails3 and Symfony2 projects.
 
+## Requirements
+* some gems but dependencies are managed automatically when you install capistrano-isitdeployed
+* ruby1.x**-dev** (otherwise you will get `no such file to load -- mkmf (LoadError)`)
+* log level must display `logger.trace`
+
 ## Installation
 
 * install the gem
 * create a project on [www.isitdeployed.com](http://www.isitdeployed.com) with a valid email address
-* you will receive the instructions to enable capistrano-isitdeployed in you deploy.rb
+* you will receive the instructions to enable capistrano-isitdeployed in your project
 
 ## Detailed installation
 
@@ -34,14 +39,14 @@ and run:
 `gem install capistrano-isitdeployed`  
 (you may have to use sudo)
 
-### 2. at the top of `config/deploy.rb`, add:  
-`require 'capistrano/isitdeployed'`
+### 2. at the top of `config/deploy.rb`, add the lines you received on your email:  
+    # isitdeplopyed settings
+    require 'capistrano/isitdeployed'
+    set :isitdeployed_project_id, 'x'
+    set :isitdeployed_api_secret, 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
-### 3. generate config file running:
-`cap isit:setup`
-
-### 4. edit `config/isitdeployed.yml` config file
-Configure `project_id` and `api_secret` with values received on your email
+### 3. Deploy!
+cap deploy
 
 ## Contributing
 
--- lib/capistrano/isitdeployed.rb
+++ lib/capistrano/isitdeployed.rb
@@ -18,7 +18,7 @@ module Capistrano
 
     def self.load_into(configuration)
       configuration.load do
-  
+
         # task hooks
         before 'deploy',               'isit:deploy'
         after  'deploy',               'isit:status:success'
@@ -28,57 +28,34 @@ module Capistrano
 
         # task definitions
         namespace :isit do
-          # TODO: generate a pre-configured file using command line arguments
-          # cap isit-setup -project_id=x -api_secret=xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
-          desc "Creates a configuration file"
-          task :setup do
-            if File.exists?(CONFIG_DEST)
-              logger.trace "WARNING: A configuration file already exists. Remove it before running setup again."
-            else 
-              path      = File.dirname(File.expand_path(__FILE__))
-              source    = path + '/isitdeployed/' + CONFIG_SRC
-              dest      = CONFIG_DEST
-              FileUtils.cp source, dest
-              logger.trace "IsItDeployed > #"
-              logger.trace "IsItDeployed > # A configuration file '#{dest}' has been created."
-              logger.trace "IsItDeployed > # Please edit this file before your next deploy."
-              logger.trace "IsItDeployed > #"
-            end
-          end
 
           desc "Creates a new deploy using the API"
           task :deploy do
-            if File.exists?(CONFIG_DEST)
-              config  = Capistrano::Isitdeployed.load_user_config
-              started = Capistrano::Isitdeployed.timestamp
-              set(:stage, "my platform") unless exists?(:stage)
-              RestClient.post(ENDPOINT + "/p/#{config['project_id']}/d", JSON.generate({ :status => 1, :platform => "#{stage}", :release => "#{release_name}", :started => started, :token => config['api_secret'], :version => VERSION }), :content_type => :json, :accept => :json, :timeout => 5, :open_timeout => 5){ |response, request, result| response
-                case response.code
-                when 201
-                  logger.trace "IsItDeployed > New deploy created for #{application} to #{stage} with version: #{release_name}"
-                  logger.trace "IsItDeployed > URL is #{ENDPOINT}/p/#{config['project_id']}"
-                  json = JSON.parse(response)
-                  if json['latest_version'] > VERSION
-                    logger.trace "IsItDeployed > WARNING: Your 'capistrano-isitdeployed' gem is outdated. Consider upgrade to #{json['latest_version']}"                  
-                  end
-                  set(:isitdeployed_did,     json['deploy_id'])
-                  set(:isitdeployed_started, started)
-                  set(:isitdeployed_created, 1)
-                when 401
-                  logger.trace "IsItDeployed > ERROR: Invalid 'api_secret'. Check your '#{CONFIG_DEST}'"                
-                  set(:isitdeployed_created, 0)
-                when 404
-                  logger.trace "IsItDeployed > ERROR: Invalid 'project_id'. Check your '#{CONFIG_DEST}'"                
-                  set(:isitdeployed_created, 0)
-                else
-                  logger.trace "IsItDeployed > ERROR: Can't create deploy, server returned code #{response.code}"
-                  set(:isitdeployed_created, 0)
+            started = Capistrano::Isitdeployed.timestamp
+            set(:stage, "my platform") unless exists?(:stage)
+            RestClient.post(ENDPOINT + "/p/#{isitdeployed_project_id}/d", JSON.generate({ :status => 1, :platform => "#{stage}", :release => "#{release_name}", :started => started, :token => isitdeployed_api_secret, :version => VERSION }), :content_type => :json, :accept => :json, :timeout => 5, :open_timeout => 5){ |response, request, result| response
+              case response.code
+              when 201
+                logger.trace "IsItDeployed > New deploy created for #{application} to #{stage} with version: #{release_name}"
+                logger.trace "IsItDeployed > URL is #{ENDPOINT}/p/#{isitdeployed_project_id}"
+                json = JSON.parse(response)
+                if json['latest_version'] > VERSION
+                  logger.trace "IsItDeployed > WARNING: Your 'capistrano-isitdeployed' gem is outdated. Consider upgrade to #{json['latest_version']}"                  
                 end
-              }
-            else 
-              logger.trace "IsItDeployed > WARNING: No configuration file found, please run cap 'isit:setup'"
-              set(:isitdeployed_created, 0)
-            end
+                set(:isitdeployed_did,     json['deploy_id'])
+                set(:isitdeployed_started, started)
+                set(:isitdeployed_created, 1)
+              when 401
+                logger.trace "IsItDeployed > ERROR: Invalid 'isitdeployed_api_secret'. Check your 'deploy.rb' file"                
+                set(:isitdeployed_created, 0)
+              when 404
+                logger.trace "IsItDeployed > ERROR: Invalid 'isitdeployed_project_id'. Check your 'deploy.rb' file"                
+                set(:isitdeployed_created, 0)
+              else
+                logger.trace "IsItDeployed > ERROR: Can't create deploy, server returned code #{response.code}"
+                set(:isitdeployed_created, 0)
+              end
+            }
           end
 
           namespace :status do
@@ -96,17 +73,16 @@ module Capistrano
           desc "Updates deploy status using the API (success or rollback)"
             task :update do
             if isitdeployed_created === 1
-              config    = Capistrano::Isitdeployed.load_user_config
               stopped   = Capistrano::Isitdeployed.timestamp
               duration  = stopped.to_i - isitdeployed_started.to_i
-              RestClient.put(ENDPOINT + "/p/#{config['project_id']}/d/#{isitdeployed_did}", JSON.generate({ :status => isitdeployed_status, :stopped => stopped, :duration => duration, :token => config['api_secret'] }), :content_type => :json, :accept => :json, :timeout => 5, :open_timeout => 5){ |response, request, result| response
+              RestClient.put(ENDPOINT + "/p/#{isitdeployed_project_id}/d/#{isitdeployed_did}", JSON.generate({ :status => isitdeployed_status, :stopped => stopped, :duration => duration, :token => isitdeployed_api_secret }), :content_type => :json, :accept => :json, :timeout => 5, :open_timeout => 5){ |response, request, result| response
                 case response.code
                 when 204
                   logger.trace "IsItDeployed > Deploy ##{isitdeployed_did} has been updated for #{application} to #{stage}."
                 when 401
-                  logger.trace "IsItDeployed > ERROR: Invalid 'api_secret'. Check your '#{CONFIG_DEST}'"                
+                  logger.trace "IsItDeployed > ERROR: Invalid 'isitdeployed_api_secret'. Check your 'deploy.rb' file"                
                 when 404
-                  logger.trace "IsItDeployed > ERROR: Invalid 'project_id'. Check your '#{CONFIG_DEST}'"                
+                  logger.trace "IsItDeployed > ERROR: Invalid 'isitdeployed_project_id'. Check your 'deploy.rb' file"                
                 else
                   logger.trace "IsItDeployed > ERROR: Can't create deploy, server returned #{response.code}"
                 end
